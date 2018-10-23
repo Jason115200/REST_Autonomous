61c012e1b446be73017169147cae627f40c5601c
Jason115200 <arbiter774@gmail.com>
2018-10-23 09:19:06 -0700

    Update Killswitch.py

--- Code/Killswitch.py
+++ Code/Killswitch.py
@@ -1,12 +1,16 @@
-import Adafruit_BBIO.GPIO as GPIO
-import time
+#import Adafruit_BBIO.GPIO as GPIO
+#import time
 
-GPIO.setup("P8_14", GPIO.IN)
-while 1:
-    if GPIO.input("P8_14"):
-        print("HIGH")
-        answer = raw_input ("ask me something: ")
-        print answer
-    else:
-        print("LOW")
-        break
+#killswitch = "P8_14"
+#GPIO.setup(killswitch, GPIO.IN)
+
+#while 1:
+#    if GPIO.input("P8_14"):
+#        print("HIGH")
+#        answer = raw_input ("ask me something: ")
+#        print answer
+#    else:
+#        print("LOW")
+#        break
+
+re do this
